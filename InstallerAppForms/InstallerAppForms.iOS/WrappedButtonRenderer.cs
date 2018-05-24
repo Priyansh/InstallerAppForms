ce9fca8d047e4a41a202a17881e0d8ba24823cd7
PriyanshS <priyansh.shah@mail.com>
2018-05-24 18:01:52 -0400

    Adding buttonTextWrap for iOS

---
+++ InstallerAppForms/InstallerAppForms.iOS/WrappedButtonRenderer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+namespace InstallerAppForms.iOS
+{
+    public class WrappedButtonRenderer : ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+            Control.TitleEdgeInsets = new UIEdgeInsets(4, 4, 4, 4);
+            Control.TitleLabel.LineBreakMode = UILineBreakMode.WordWrap;
+            Control.TitleLabel.TextAlignment = UITextAlignment.Center;
+        }
+    }
+}
