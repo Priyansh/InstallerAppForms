efc61f8aae27bebc89ff391a4e611ec141a5206c
PriyanshS <priyansh.shah@mail.com>
2018-06-28 15:47:35 -0400

    Added Camera Plugins and change UI in PhotoGallery

--- InstallerAppForms/InstallerAppForms/PhotoGallery.xaml.cs
+++ InstallerAppForms/InstallerAppForms/PhotoGallery.xaml.cs
@@ -32,9 +32,13 @@ namespace InstallerAppForms
             BindingContext = this.selectedIndividualRoom;
         }
 
-        private void BtnPhoto_Clicked(object sender, EventArgs e)
+        private async void BtnPhoto_Clicked(object sender, EventArgs e)
         {
-
+            var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
+            if (photo != null) {
+                PhotoImage.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+                PhotoImage1.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+            }
         }
     }
 }
