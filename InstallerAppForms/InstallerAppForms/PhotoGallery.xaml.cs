0fba1e68c62772ca248906cf60e89665ceef92a5
PriyanshS <priyansh.shah@mail.com>
2018-06-28 20:23:48 -0400

    Updated GridLayout for PhotoGallery

--- InstallerAppForms/InstallerAppForms/PhotoGallery.xaml.cs
+++ InstallerAppForms/InstallerAppForms/PhotoGallery.xaml.cs
@@ -14,6 +14,7 @@ namespace InstallerAppForms
     {
         int installerId;
         IndividualRoomCS selectedIndividualRoom;
+        int row = 0;  int col = 0;
         public PhotoGallery(int getInstallerId, IndividualRoomCS individualRoom)
         {
             InitializeComponent();
@@ -35,9 +36,25 @@ namespace InstallerAppForms
         private async void BtnPhoto_Clicked(object sender, EventArgs e)
         {
             var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions() { });
-            if (photo != null) {
-                PhotoImage.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
-                PhotoImage1.Source = ImageSource.FromStream(() => { return photo.GetStream(); });
+            if (photo != null)
+            {
+                var slPhotos = new StackLayout
+                {
+                    Orientation = StackOrientation.Horizontal
+                };
+                var alpha = new Image { Source = ImageSource.FromStream(() => { return photo.GetStream(); }),
+                                        WidthRequest = 200,
+                                        HeightRequest = 200
+                                       };
+                if(col == 0) grdLayout.RowDefinitions.Add(new RowDefinition { Height = new GridLength(200, GridUnitType.Absolute) });
+                
+                grdLayout.ColumnDefinitions.Add(new ColumnDefinition {
+                    Width = new GridLength(200, GridUnitType.Absolute)
+                });
+
+                grdLayout.Children.Add(alpha, col, row);
+                col++;
+                if (col == 2) { col = 0; row++; }
             }
         }
     }
